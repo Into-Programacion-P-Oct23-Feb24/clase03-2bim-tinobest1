ae25ac347bdf4d075e4024bcf89b22a5a5eacb5c
TinoBest <sayayinleninlenin@hotmail.com>
2023-12-20 15:46:07 -0500

    -acero

---
+++ Ejemplosc3/paquete05/src/paquete05/Paquete05.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paquete05;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Paquete05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int[] arreglo1 = new int[2];
+        for (int i = 1; 1 < arreglo1.length; i++) {
+            System.out.println(arreglo1[i]);
+        }
+
+    }
+
+}
